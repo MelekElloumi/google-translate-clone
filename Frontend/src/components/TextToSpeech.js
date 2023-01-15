050b7d99c6c9b6d7688e3d7e54d044b313a1c442
Melek Elloumi <melek.elloumi@insat.ucar.tn>
2023-01-15 01:45:33 +0100

    Added text to speech

--- Frontend/src/components/TextToSpeech.js
+++ Frontend/src/components/TextToSpeech.js
@@ -1,28 +1,35 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
-import AudioPlayer from 'react-audio-player';
+import ReactPlayer from 'react-audio-player';
 
-function TextToSpeech({ text },{ language }) {
+function TextToSpeech({ text, language }) {
     //const [text, setText] = useState('');
     const [audioUrl, setAudioUrl] = useState(null);
-  
+
     async function handleSubmit() {
       // Call the REST API endpoint with the text
-      const response = await fetch("/api/text-to-speech?text=${texts}");
-      const audioUrl = await response.text();
-  
+      /*const response = await fetch("http://127.0.0.1:5000/tts?text=${text}&language=${language}");
+      const audioUrl = await response.text();*/
+      console.log(text,language);
+      axios.get(`http://127.0.0.1:5000/tts?text=${text}&language=${language}`, {responseType: 'arraybuffer'})
+      .then(response => {
+        const blob = new Blob([response.data], { type: 'audio/mp3' });
+        setAudioUrl(URL.createObjectURL(blob));
+      });
       // Update the state with the URL of the audio file
-      setAudioUrl(audioUrl);
+      //setAudioUrl(audioUrl);
     }
   
     return (
       <div>
-        <button onClick={handleSubmit}><i className='fas fa-volume-up text-secondary'></i>
+        <button onClick={handleSubmit} style={{backgroundColor:"white",display: "inline-block",border: "none"}}><i className='fas fa-volume-up text-secondary'></i>
         </button>
         {audioUrl && (
-          <AudioPlayer
-            src={audioUrl}
-            autoPlay
-          />
+           <ReactPlayer
+           src={audioUrl}
+           type="audio/mp3"
+           autoPlay
+            />
         )}
       </div>
     );
--- TextToSpeechAPI/text_to_speech_app.py
+++ TextToSpeechAPI/text_to_speech_app.py
@@ -1,19 +1,33 @@
-from flask import Flask,jsonify
+from flask import Flask, request, send_file, Response
+from flask_cors import CORS
 from waitress import serve
 import os
 import json
-
+from gtts import gTTS
+import io
 CONFIG_FILE="config.json"
 
 def create_app(name):
 
     app = Flask(name)
+    CORS(app)
 
     @app.route('/tts', methods=['GET'])
-    def products():
-        response=jsonify([0,1])
-        return response
+    def textToSpeech():
+        text=request.args.get('text')
+        language=request.args.get('language')
+        if not text or not language:
+            return Response(status=204)
+        print(text,language)
+        tts = gTTS(text=text, lang=language)
+        tts.save("temp_audio/tts.mp3")
 
+        with open("temp_audio/tts.mp3", 'rb') as bites:
+            return send_file(
+                io.BytesIO(bites.read()),
+                attachment_filename='tts.mp3',
+                mimetype='audio/mp3'
+            )
 
     return app
 
@@ -24,12 +38,12 @@ def main(production):
     SECRET_KEY = os.urandom(24)
     app.secret_key = SECRET_KEY
     f = open("secrets.txt", "w")
-    f.write("Tts app secret key: " + str(SECRET_KEY))
+    f.write("Text to Speech app secret key: " + str(SECRET_KEY))
     f.close()
     if production:
         serve(app, host="127.0.0.1", port=8080)
     else:
-        app.run(host='0.0.0.0', port=5000)
+        app.run(port=5000)
 
 
 if __name__ == '__main__':
